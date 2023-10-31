445db4517fbab0ff112ed7475842da448ca5dd04
vhboico <hboico@hotmail.com>
2023-10-31 09:34:05 -0300

    final commit

--- app/src/main/java/com/example/chat/MainActivity.kt
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -24,9 +24,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ChatTheme {
                 LayoutChat()
-            }
+
         }
     }
 }
